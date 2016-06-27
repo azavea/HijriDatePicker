f4d6b785d615c45f71777523fe32633c14e9859d
Kathryn Killebrew <kkillebrew@azavea.com>
2016-06-27 16:27:47 -0400

    Support min SDK 14.
    Also bump support library versions.

--- example/src/main/java/net/alhazmy13/hijricalenderexample/MainActivity.java
+++ example/src/main/java/net/alhazmy13/hijricalenderexample/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements HijriCalendarView
                         .setOnDateSetListener(MainActivity.this)
                         .setMinMaxHijriYear(1430,1450)
                         .setMinMaxGregorianYear(2013,2020)
+                        //.setUILanguage(HijriCalendarDialog.Language.Arabic)
                         .setMode(HijriCalendarDialog.Mode.Hijri)
                         .setDefaultHijriDate(8, 0, 1437)//months start from 0
                         .setEnableScrolling(false)
